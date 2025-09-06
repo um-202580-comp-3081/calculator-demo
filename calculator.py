0c06db61ae9f0f6028bda162a352494b45e11aee
Katie Bridson <kbridson@memphis.edu>
2025-09-05 19:54:42 -0500

    Takes an expression arg from command line, error checks not enough args

--- calculator.py
+++ calculator.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python3
+import sys
+
+
+def usage():
+    print(
+        """
+usage: calculator.py [-v] EXPR
+       EXPR: valid mathematical expression to evaluate
+options:
+  -v            verbose mode
+""",
+        file=sys.stderr,
+    )
+
+
+"""
+Test Cases
+
+Expected Success:
+./calculator.py "1 + 2"
+
+Expected Failure:
+./calculator.py
+
+"""
+
+if len(sys.argv) < 2:
+    print("error: not enough arguments")
+    usage()
+    sys.exit(1)
+
+
+expr = sys.argv[1]
 
 
 def calculate(expr):
     print(eval(expr))
 
 
-expr = "1 + 2"
 calculate(expr)
